563ca3fa466ebc1c0492efebf5bf2669517b6dad
César Damasceno <64749161+ManeMaria@users.noreply.github.com>
2023-08-15 23:03:45 -0300

    feat: initial configs

---
+++ src/components/input/index.tsx
@@ -0,0 +1,18 @@
+import React, { InputHTMLAttributes } from 'react';
+
+type InputProps = {
+  label: string;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+export const Input: React.FC<InputProps> = ({
+  label,
+  ...rest
+}) => {
+  return (
+    <div className='grid'>
+      <label className='text-lg font-semibold'>{label}</label>
+      <input className='rounded-2xl border-1 border-solid h-14 w-60 border-transparent shadow-3xl shadow-black focus:outline-none  focus:border-black border-2 focus:transition-all px-3 text-xl'   {...rest} />
+    </div>
+  )
+}
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,32 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          100: '#F2E6F7',
+          200: '#EAACC4',
+          300: '#FECBD8',
+          400: '#DFC9FA',
+        },
+        secondary: {
+          100: '#F4F4F4',
+          200: '#D9D9D9',
+          300: '#BDBDBD',
+          400: '#6B8989'
+        },
+        tertiary: {
+          yellow: '#FEDA79',
+          green: '#CCE5E5'
+        },
+        black: '#000',
+
       },
+
+      boxShadow: {
+        '3xl': '8px 7px 1px #000',
+      }
+
+
     },
   },
   plugins: [],
